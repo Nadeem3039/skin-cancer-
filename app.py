443427e0a2a22f38ef6cc1f551dbb014dbf1ba75
Nadeem3039 <bme115uet@gmail.com>
2025-03-25 00:31:40 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ st.write("Upload an image for analysis.")
 uploaded_file = st.file_uploader("Choose a file", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
-    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+   st.image(uploaded_file, caption="Uploaded Image", use_container_width=True)
